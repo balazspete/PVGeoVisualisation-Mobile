73fb556ffeb36afd17dd3fd71b93f76018ba5c8e
Balazs Pete <bazsip@gmail.com>
2014-05-24 00:14:49 +0100

    Add dependencies
    Redland-ObjC, GroundControl, SkyLab

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '7.0' 
+pod 'GroundControl', '~> 2.1.0'
+pod 'SkyLab', '~> 1.0.0'
---
+++ Pods/Pods-SkyLab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkyLab : NSObject
+@end
+@implementation PodsDummy_Pods_SkyLab
+@end
